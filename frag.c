12477ae32857a8e90c09d8e9dfb397de7d31e396
cloakzzzy <99138297+cloakzzzy@users.noreply.github.com>
2022-10-27 14:28:31 +0100

    Update frag.c

--- frag.c
+++ frag.c
@@ -1,9 +1,12 @@
 /*
 
-       a                                b                                    c
-       ⬇                                ⬇                                    ⬇
-
-(B.x^2 + B.y^2 + B.z^2)t^2 + (2(A.x B.x + A.y B.y + A.z + B.z))t + (A.x^2 + A.y^2 + A.z^2 - r^2) = 0
+                                                                          
+a:
+((B.x)^2 + (B.y)^2 + (B.z)^2)t^2 + 
+b:
+(2(A.x B.x + A.y B.y + A.z B.z))t + 
+c:
+(A.x^2 + A.y^2 + A.z^2 - r^2) = 0
 
 P = Bt (can figure out any point along a line)
 
@@ -22,30 +25,39 @@ Quadratic formula:
 
 */
 
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 pix = fragCoord/iResolution.xy * 2.0 - 1.0;
     float aspectratio = iResolution.x / iResolution.y;
     pix.x *= aspectratio;
-
-    float radius = 0.5f;
-    vec3 rayDirection = vec3(pix.x, pix.y , 1.0f);
-    vec3 rayOrigin = vec3(1.f, 0.f, -2.f);
+    const vec4 backcol = vec4(0.f,0.f,0.f,0.f);
+    
+    vec3 spherepos = vec3(0.0f ,0.0f ,2.0f);
     vec3 spherecol = vec3(0.f,1.f,1.f);
     
-    float a = dot(rayDirection, rayDirection);
-    float b = 2.0f * dot(rayOrigin, rayDirection);
-    float c = dot(rayOrigin,rayOrigin) - radius * radius;
+
+    float radius = 1.0f;
+    vec3 rayDir = vec3(pix.x, pix.y , 1.0f);
+    //campos
+    vec3 rayOrigin = vec3(0.f, 0.0f, 0.f);
+    
+    float a = dot(rayDir, rayDir); //constant 
+    float b = 2.0f * dot(rayOrigin - spherepos, rayDir); //differs for each sphere 
+    float c = dot(rayOrigin - spherepos,rayOrigin - spherepos) - radius * radius; //differs for each sphere
     
     float discriminant = b * b - 4.0f * a * c;
+
     
-    float t0 = -b + (sqrt((discriminant) / 2.0f*a));
-    vec3 hitpoint = rayOrigin + rayDirection * t0;
-    
-    if (discriminant >= 0.f){
-        fragColor = vec4(hitpoint , 1.f);
+    if (discriminant >= 0.f)
+    {
+        float t0 = -b - (sqrt((discriminant) / 2.0f*a));
+
+        vec3 hitpoint = rayOrigin + rayDir * t0;
+
+        fragColor = vec4(hitpoint, 1.f);
+        
+        return;
     }
-    
+    fragColor = (backcol);
     
 }
