1b60ad12f08c79d535dade0cb94ccb907f5c9340
cloakzzzy <99138297+cloakzzzy@users.noreply.github.com>
2022-10-31 15:44:42 +0000

    Update frag.c

--- frag.c
+++ frag.c
@@ -23,52 +23,81 @@ we need to solve for (t)
 Quadratic formula:
 -b += sqrt((b^2 - 4*a*c) \ 2a)
 
-conditions:
-if ray hits red sphere
-if ray hits blue sphere
-if ray hits both blue and red sphere
+rotation:
+point.x = (px * cos(deg * RAD)) - (py * sin(deg * RAD)) + cx; 
+point.z = (px * sin(deg * RAD)) + (py * cos(deg * RAD)) + cy;
 
 */
-
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 pix = fragCoord/iResolution.xy * 2.0 - 1.0;
     float aspectratio = iResolution.x / iResolution.y;
     pix.x *= aspectratio;
-    const vec4 backcol = vec4(0.f,0.f,0.f,0.f);
     
-    vec3 spherepos [] = vec3[](vec3(6.f,0.f,10.f), vec3(0.0f ,0.0f ,2.0f));
-    vec3 spherecol []= vec3[](vec3(0.f,1.f,1.f), vec3(1.f,0.f,0.f));
-    //                            blue                   red
-    float t [] = float[](0.f);
+    const vec3 backcol = vec3(0.f,0.f,0.f);
     
-    float radius = 1.0f;
+    float RAD = 3.14159f/180.0f;
+    
+    //float deg = iMouse.x / 5.0f;
+    float deg = 0.0f;
+    
+    vec3 rayOrigin = vec3(0.f, 2.0f, 0.f);
+    
+    float radius [] = float[](1.f,1.f,1.f);
+    vec3 spherepos [] = vec3[](vec3(3.f,0.f,5.f), vec3(0.0f ,0.0f ,2.0f),vec3( 0.5,0.5,5.f));
+    vec3 spherecol []= vec3[](vec3(0.f,0.f,1.f), vec3(1.f,0.f,0.f), vec3(0.f,1.f,0.f));
+     //                           blue                   red                green
+    float t [] = float[](0.f,0.f,0.f);
+    
+    
     vec3 rayDir = vec3(pix.x, pix.y , 1.0f);
-    vec3 rayOrigin = vec3(1.f, 1.0f, 0.f);
-    /*
-    float a = dot(rayDir, rayDir); //constant 
+    
+    float a = dot(rayDir, rayDir); //constant
     
     for (int i = 0; i < spherepos.length(); i++)
     {
+        //rotation around the xz axis
+        spherepos[i].x = ((spherepos[i].x - rayOrigin.x) * cos(deg * RAD)) - ((spherepos[i].z - rayOrigin.z) * (sin(deg*RAD)) + rayOrigin.x);
+        spherepos[i].z = ((spherepos[i].x - rayOrigin.x) * sin(deg * RAD)) + ((spherepos[i].z - rayOrigin.z) * (cos(deg*RAD)) + rayOrigin.x);
+    
+    
         float b = 2.0f * dot(rayOrigin - spherepos[i], rayDir); 
-        float c = dot(rayOrigin - spherepos[i],rayOrigin - spherepos[i]) - radius * radius;
+        float c = dot(rayOrigin - spherepos[i],rayOrigin - spherepos[i]) - radius[i] * radius[i];
         
         float discriminant = b * b - 4.0f * a * c;
         
         if (discriminant >= 0.f)
         {
-            t[i] = -b - (sqrt((discriminant) / 2.0f*a));
+            t[i] = -b - (sqrt((discriminant) / 2.0f * a));
         }
         else
         {
             t[i] = 1000000000.0f;
         }
+        
     }
     
-    int ind;
+    int ind = 0;
     float minimum;
     
+    bool eq = true;
+    
+    //if there are no intersections make it white ( if all values in t array are the same
+    for (int i = 0 ; i < t.length(); i++)
+    {
+        //if values are not the same 'eq' = false and break the loop
+        if (t[i] != t[0]) {eq = false; break;}
+    }
+    
+    if (eq == true)
+    {
+        //if no intersection then make the pixel equal to the backcolor
+        fragColor = vec4(vec3(backcol), 0.f);
+        return;
+    }
+    
+    //finding the smallest value in t array and storing that index in the 'ind' variable
     for (int i = 0; i < t.length(); i++)
     {
     
@@ -82,63 +111,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
             ind = i;
         }
     }
-  
     
-    fragColor = vec4(spherecol[ind], 1.f);
-    */
-    
-    
-    float a = dot(rayDir, rayDir); //constant 
-    float b = 2.0f * dot(rayOrigin - spherepos[0], rayDir); //differs for each sphere 
-    float c = dot(rayOrigin - spherepos[0],rayOrigin - spherepos[0]) - radius * radius; //differs for each sphere
-    
-    float discriminant = b * b - 4.0f * a * c;
-    //float t0 = 10000000.0f;
-    float t0;
-    float t1;
-    
-    //blue sphere contact
-    if (discriminant >= 0.f)
-    {
-        
-        t0 = -b - (sqrt((discriminant) / 2.0f*a));
-        //fragColor = vec4(t0-round(t0));
-    }
-    else
-    {
-        t0 = 1000000000.0f;
-    }
-    
-    float b2 = 2.0f * dot(rayOrigin - spherepos[1], rayDir); //differs for each sphere 
-    float c2 = dot(rayOrigin - spherepos[1],rayOrigin - spherepos[1]) - radius * radius; //differs for each sphere
-    
-    float discriminant2 = b2 * b2 - 4.0f * a * c2; 
-
-    //red sphere contact
-    if (discriminant2 >= 0.f)
-    {
-        
-        t1 = -b2 - (sqrt((discriminant2) / 2.0f*a));
-    }
-    else
-    {
-        t1 = 1000000000.0f;
-    }
-   
-    
-    ///if blue is before red
-    if (t0 < t1)
-    {
-        
-        fragColor = vec4(spherecol[0], 1.f);   
-    }
-    //red is before blue
-    else if(t0 > t1)
-    {
-        fragColor = vec4(spherecol[1], 1.f);
-    }
-    
-    
-    
-    
+    //setting the fragColor to that color
+    //fragColor = vec4(vec3(spherecol[ind]), 1.f);
+    fragColor = vec4(vec3(rayOrigin * t[ind] - round(rayOrigin * t[ind])), 1.f);
 }
